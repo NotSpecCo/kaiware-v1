b302d82094db926323dd553252f79659c8906aa3
Garrett Downs <garredow@outlook.com>
2021-12-03 12:00:05 -0500

    add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
@@ -9,14 +10,25 @@ import { Category } from './routes/Category';
 import { Home } from './routes/Home';
 import { getDeviceInfo } from './services/device';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 600000,
+    },
+  },
+});
+
 export function AppWrapper(): JSX.Element {
   return (
     <HashRouter>
-      <SettingsProvider>
-        <DeviceProvider>
-          <App />
-        </DeviceProvider>
-      </SettingsProvider>
+      <QueryClientProvider client={queryClient}>
+        <SettingsProvider>
+          <DeviceProvider>
+            <App />
+          </DeviceProvider>
+        </SettingsProvider>
+      </QueryClientProvider>
     </HashRouter>
   );
 }
