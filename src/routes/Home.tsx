c26baefac91b9c281ada7103870da793befe9b15
Garrett Downs <garredow@outlook.com>
2021-12-03 15:09:07 -0500

    add basic home view

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Typography } from '../ui-components/Typography';
+import { View, ViewContent, ViewHeader } from '../ui-components/view';
 
 export function Home(): JSX.Element {
   return (
-    <div>
-      <div>Home</div>
-    </div>
+    <View>
+      <ViewHeader>
+        <Typography type="titleLarge" padding="none">
+          Home
+        </Typography>
+      </ViewHeader>
+      <ViewContent>Home view</ViewContent>
+    </View>
   );
 }
