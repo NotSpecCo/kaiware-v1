dd375a86356740ac1d2e6f0b2f8447f5e94aafe9
Garrett Downs <garredow@outlook.com>
2021-12-06 16:26:59 -0500

    add secondary button type

--- src/ui-components/Button.tsx
+++ src/ui-components/Button.tsx
@@ -6,16 +6,18 @@ import styles from './Button.module.css';
 
 type Props = ComponentBaseProps &
   ClickableProps & {
+    type?: 'primary' | 'secondary';
     text: string;
     disabled?: boolean;
     fullWidth?: boolean;
   };
 
-export function Button(props: Props): JSX.Element {
+export function Button({ type = 'primary', ...props }: Props): JSX.Element {
   return (
     <button
       className={joinClasses(
         styles.root,
+        styles[type],
         ifClass(props.fullWidth, styles.fullWidth),
         props.className
       )}
