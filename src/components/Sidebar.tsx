be375a50f84597f0e5dae8d13011f30b2466cd28
Garrett Downs <garredow@outlook.com>
2021-12-08 13:56:03 -0500

    fix sidebar issue on small windows

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,43 +38,45 @@ export function Sidebar(): JSX.Element {
   return (
     <div className={styles.root}>
       <div className={styles.titlebar} />
-      <SidebarItem primaryText="Home" onClick={() => history.push(`/`)} />
-      <Typography type="titleSmall">Apps</Typography>
-      {categories.map((a) => (
-        <SidebarItem
-          key={a.id}
-          primaryText={a.name}
-          onClick={() => history.push(`/category/${a.id}`)}
-        />
-      ))}
-      <div className={styles.spacer} />
-      <Typography type="titleSmall">System</Typography>
-      <SidebarItem primaryText="Settings" onClick={() => history.push(`/settings`)} />
-      <SidebarItem primaryText="About" />
-      <Typography type="titleSmall">Device</Typography>
-      {device ? (
-        <SidebarItem
-          primaryText={device.name}
-          secondaryText="Connected"
-          onClick={() => history.push(`/device`)}
-        >
-          <IconButton
-            className={styles.btnRefresh}
-            icon="refresh"
-            animation="spin"
-            onClick={() => refreshDevice()}
+      <div className={styles.items}>
+        <SidebarItem primaryText="Home" onClick={() => history.push(`/`)} />
+        <Typography type="titleSmall">Apps</Typography>
+        {categories.map((a) => (
+          <SidebarItem
+            key={a.id}
+            primaryText={a.name}
+            onClick={() => history.push(`/category/${a.id}`)}
           />
-        </SidebarItem>
-      ) : (
-        <SidebarItem primaryText="No Device">
-          <IconButton
-            className={styles.btnRefresh}
-            icon="refresh"
-            animation="spin"
-            onClick={() => refreshDevice()}
-          />
-        </SidebarItem>
-      )}
+        ))}
+        <div className={styles.spacer} />
+        <Typography type="titleSmall">System</Typography>
+        <SidebarItem primaryText="Settings" onClick={() => history.push(`/settings`)} />
+        <SidebarItem primaryText="About" />
+        <Typography type="titleSmall">Device</Typography>
+        {device ? (
+          <SidebarItem
+            primaryText={device.name}
+            secondaryText="Connected"
+            onClick={() => history.push(`/device`)}
+          >
+            <IconButton
+              className={styles.btnRefresh}
+              icon="refresh"
+              animation="spin"
+              onClick={() => refreshDevice()}
+            />
+          </SidebarItem>
+        ) : (
+          <SidebarItem primaryText="No Device">
+            <IconButton
+              className={styles.btnRefresh}
+              icon="refresh"
+              animation="spin"
+              onClick={() => refreshDevice()}
+            />
+          </SidebarItem>
+        )}
+      </div>
     </div>
   );
 }
