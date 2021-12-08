a4b63ccd42ba1e91f3971600ed931854b9efe2e0
Simon Laux <mobile.info@simonlaux.de>
2021-12-08 20:48:24 +0100

    get rid of `(window as any)`, type the functions provided by the preload instead

---
+++ src/globals.d.ts
@@ -0,0 +1,23 @@
+declare global {
+  interface Window {
+    // see preload.ts for implementation
+    electron: {
+      kaiDevice: {
+        getInfo: () => Promise<DeviceInfo>;
+        getRunningApps: () => Promise<DeviceApp[]>;
+        getInstalledApps: () => Promise<DeviceApp[]>;
+        installApp: (url: string) => Promise<DeviceApp>;
+        installLocalApp: (url: string) => Promise<DeviceApp>;
+        uninstallApp: (appId: string) => Promise<void>;
+        launchApp: (appId: string) => Promise<void>;
+        closeApp: (appId: string) => Promise<void>;
+      };
+      browser: {
+        openUrl: (url: string) => Promise<void>;
+        downloadUrl: (url: string) => Promise<void>;
+      };
+    };
+  }
+}
+
+export {};
--- src/services/browser.ts
+++ src/services/browser.ts
@@ -1,6 +1,6 @@
 export function openUrl(url: string): Promise<void> {
-  return (window as any).electron.browser.openUrl(url);
+  return window.electron.browser.openUrl(url);
 }
 export function downloadUrl(url: string): Promise<void> {
-  return (window as any).electron.browser.downloadUrl(url);
+  return window.electron.browser.downloadUrl(url);
 }
